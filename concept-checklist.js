766fb3d0610623e61412927d68e01e61c0e5ecab
CoderMahabub <codermahabub@gmail.com>
2021-08-09 23:52:17 +0600

    JS Basic Checklist

---
+++ concept-checklist.js
@@ -0,0 +1,43 @@
+// 1. Declare Three Varialbe
+// 1.1. Number Type Variable
+var number = 12;
+var number2 = 12.5;
+// 1.2. String Type Variable
+const myName = "Mahabub Azam";
+const myFriendName = "Amirul Islam";
+// 1.3. Boolean Type Variable
+var isYouPassed = true;
+var isYouFailed = false;
+
+// 2. Variable let and const
+// 2.1. let
+let myAge = 15;
+myAge = 25;
+// 2.2 const
+console.log(myAge);
+const pi = 3.1416;
+
+// 3. Simple Math Operation(+,)
+
+// 4. comparison (< , >, ==, !=, <=, >=)
+
+// 5. Two condition Comparison | Fulfill both condition | Fulfill at least one Condition
+
+// 6. if else 
+
+// 7. while loop to display  7 -19 all numbers. Only display odd numbers including 7 to 19
+
+// 8. Declare an array. Number of elements. 4th position er element change or update. Add or Remove elements. Check whether a specific value existis in the array?
+
+// 9.Use any for loop to display every elements of an arrray
+
+// 10. you have an array of numbers. Display only the numbers bigger than 80.
+
+
+// 11. write a function that takes three numbers and return multiplication of the three numbers.
+
+
+// 12. declare an object and change any property of that object.
+
+
+
